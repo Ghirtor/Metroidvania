6fcd9e8ccb73af9a12e4bca908ba6bff4f503a63
Julien <julien@localhost.localdomain>
2018-02-18 15:04:37 +0100

    New tests and bugs of the scene module fixed

--- Tests/test_camera.ml
+++ Tests/test_camera.ml
@@ -1,10 +1,40 @@
-let cam1 = Camera.create_camera 0 0 500 500;;
-Printf.printf "The camera: {%d, %d, %d, %d}\n" (Camera.get_x cam1) (Camera.get_y cam1) (Camera.get_w cam1) (Camera.get_h cam1);;
+open OUnit2;;
 
-Printf.printf "I modify the size of the camera\n";;
-let cam2 = Camera.modify_size cam1 400 800;;
-Printf.printf "The new camera: {%d, %d, %d, %d}\n" (Camera.get_x cam2) (Camera.get_y cam2) (Camera.get_w cam2) (Camera.get_h cam2);;
+let field_width = 10000;;
+let field_height = 2000;;
 
-Printf.printf "I'll move the camera\n";;
-let cam3 = Camera.move_camera cam1 1000 500 1000 600 20 60;;
-Printf.printf "The new camera: {%d, %d, %d, %d}\n" (Camera.get_x cam3) (Camera.get_y cam3) (Camera.get_w cam3) (Camera.get_h cam3);;
+let cam = Camera.create_camera 0 2000 720 1280;;
+
+assert_equal (Camera.get_h cam) 720;;
+assert_equal (Camera.get_w cam) 1280;;
+Printf.printf  "The camera is created\n";;
+
+let cam2 = Camera.modify_size cam 1080 1920;;
+assert_equal (Camera.get_h cam2) 1080;;
+assert_equal (Camera.get_w cam2) 1920;;
+Printf.printf "The camera is modified\n";;
+
+let cam3 = Camera.move_camera cam2 4950 1450 10000 2000 100 100;;
+assert_equal (Camera.get_x cam3) 4040;;
+assert_equal (Camera.get_y cam3) 920;;
+Printf.printf "The camera has moved\n";;
+
+let cam4 = Camera.move_camera cam2 9950 1950 10000 2000 100 100;;
+assert_equal (Camera.get_x cam4) 8080;;
+assert_equal (Camera.get_y cam4) 920;;
+Printf.printf "The camera is in the low right corner\n";;
+
+let cam5 = Camera.move_camera cam2 9950 0 10000 2000 100 100;;
+assert_equal (Camera.get_x cam5) 8080;;
+assert_equal (Camera.get_y cam5) 0;;
+Printf.printf "The camera is in the high right corner\n";;
+
+let cam6 = Camera.move_camera cam2 0 0 10000 2000 100 100;;
+assert_equal (Camera.get_x cam6) 0;;
+assert_equal (Camera.get_y cam6) 0;;
+Printf.printf "The camera is in the high left corner\n";;
+
+let cam7 = Camera.move_camera cam2 0 1950 10000 2000 100 100;;
+assert_equal (Camera.get_x cam7) 0;;
+assert_equal (Camera.get_y cam7) 920;;
+Printf.printf "The camera is in the low left corner\n";;
